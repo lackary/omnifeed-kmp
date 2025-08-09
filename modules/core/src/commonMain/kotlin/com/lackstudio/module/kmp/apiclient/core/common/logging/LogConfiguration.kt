1dad2b1a6b2ff6eae04e9c24b5cf50c644a5c306
Yu-Chan (Henry) Huang <lackary@gmail.com>
2025-08-09 20:05:47 +0800

    refactor(logging): Change the default format for timestamp for LogConfiguration
    - Add time zone format into timestamp property.

--- modules/core/src/commonMain/kotlin/com/lackstudio/module/kmp/apiclient/core/common/logging/LogConfiguration.kt
+++ modules/core/src/commonMain/kotlin/com/lackstudio/module/kmp/apiclient/core/common/logging/LogConfiguration.kt
@@ -3,5 +3,5 @@ package com.lackstudio.module.kmp.apiclient.core.common.logging
 object LogConfiguration {
     var appTag: String = "App"
     var ktorTag: String = "KtorClient"
-    var timestampFormat: String = "yyyy-MM-dd HH:mm:ss.SSS"
+    var timestampFormat: String = "yyyy-MM-dd HH:mm:ss.SSSZ"
 }
