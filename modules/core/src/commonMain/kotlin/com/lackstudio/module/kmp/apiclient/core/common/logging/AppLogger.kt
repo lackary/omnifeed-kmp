8b372c0b7d0dbe4d28b846fa66a23239e75083a9
Yu-Chan (Henry) Huang <lackary@gmail.com>
2025-08-11 14:25:33 +0800

    feat(logging): Add tag into each log levels in AppLogger
    - Add tag for each log levels.

--- modules/core/src/commonMain/kotlin/com/lackstudio/module/kmp/apiclient/core/common/logging/AppLogger.kt
+++ modules/core/src/commonMain/kotlin/com/lackstudio/module/kmp/apiclient/core/common/logging/AppLogger.kt
@@ -1,8 +1,8 @@
 package com.lackstudio.module.kmp.apiclient.core.common.logging
 
 interface AppLogger {
-    fun debug(message: String, throwable: Throwable? = null)
-    fun info(message: String, throwable: Throwable? = null)
-    fun warn(message: String, throwable: Throwable? = null)
-    fun error(message: String, throwable: Throwable? = null)
+    fun debug(tag: String, message: String, throwable: Throwable? = null)
+    fun info(tag: String, message: String, throwable: Throwable? = null)
+    fun warn(tag: String, message: String, throwable: Throwable? = null)
+    fun error(tag: String, message: String, throwable: Throwable? = null)
 }
--- modules/core/src/commonMain/kotlin/com/lackstudio/module/kmp/apiclient/core/common/logging/AppLoggerImpl.kt
+++ modules/core/src/commonMain/kotlin/com/lackstudio/module/kmp/apiclient/core/common/logging/AppLoggerImpl.kt
@@ -3,15 +3,15 @@ package com.lackstudio.module.kmp.apiclient.core.common.logging
 import co.touchlab.kermit.Logger
 
 class AppLoggerImpl(private val kermitLogger: Logger): AppLogger {
-    override fun debug(message: String, throwable: Throwable?) =
-        kermitLogger.d(message, throwable)
+    override fun debug(tag: String, message: String, throwable: Throwable?) =
+        kermitLogger.d(tag = tag, messageString = message, throwable = throwable)
 
-    override fun info(message: String, throwable: Throwable?) =
-        kermitLogger.i(message, throwable)
+    override fun info(tag: String, message: String, throwable: Throwable?) =
+        kermitLogger.i(tag = tag, messageString = message, throwable = throwable)
 
-    override fun warn(message: String, throwable: Throwable?) =
-        kermitLogger.w(message, throwable)
+    override fun warn(tag: String, message: String, throwable: Throwable?) =
+        kermitLogger.w(tag = tag, messageString = message, throwable = throwable)
 
-    override fun error(message: String, throwable: Throwable?) =
-        kermitLogger.e(message, throwable)
+    override fun error(tag: String, message: String, throwable: Throwable?) =
+        kermitLogger.e(tag = tag, messageString = message, throwable = throwable)
 }
