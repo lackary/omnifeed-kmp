07a9cc5232e77c8f48e2d84743fdaf20a5589316
Yu-Chan (Henry) Huang <lackary@gmail.com>
2025-08-27 18:33:39 +0800

    chore(project): Rename project root name and add some libs in toml
    - Change root project group from "com" to "io".
    - Update AGP versions.
    - Add dependencies for logging (Logback), testing (JUnit, Mockito), and Compose UI.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,11 @@
 plugins {
     //trick: for the same plugin versions in all sub-modules
+    alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.compose.hot.reload) apply false
+    alias(libs.plugins.compose.multiplatform) apply false
+    alias(libs.plugins.kotlin.compose.compiler) apply false
+    alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.devtool.ksp) apply false
@@ -8,6 +13,6 @@ plugins {
 
 subprojects {
     //trick: for the same plugin versions in all sub-modules
-    group = "com.lackstudio.module.kmp.apiclient"
+    group = "io.lackstudio.module.kmp.apiclient"
     version = "0.0.1"
 }
