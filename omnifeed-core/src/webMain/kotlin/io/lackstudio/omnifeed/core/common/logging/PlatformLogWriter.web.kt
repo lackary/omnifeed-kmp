b0a96ddc83ce1128fcd18ba63d99ed78cc39be3e
Yu-Chan (Henry) Huang <lackary@gmail.com>
2026-02-02 21:43:48 +0800

    refactor(logging): switch to ConsoleWriter for web platform
    Updated the web implementation of `getPlatformLogWriter` to use `ConsoleWriter` instead of `CommonWriter`. This ensures better integration with browser developer consoles by using platform-specific logging methods.
    
    - Replaced `co.touchlab.kermit.CommonWriter` with `co.touchlab.kermit.ConsoleWriter` in `PlatformLogWriter.web.kt`.

--- omnifeed-core/src/webMain/kotlin/io/lackstudio/omnifeed/core/common/logging/PlatformLogWriter.web.kt
+++ omnifeed-core/src/webMain/kotlin/io/lackstudio/omnifeed/core/common/logging/PlatformLogWriter.web.kt
@@ -1,9 +1,9 @@
 package io.lackstudio.omnifeed.core.common.logging
 
-import co.touchlab.kermit.CommonWriter
+import co.touchlab.kermit.ConsoleWriter
 import co.touchlab.kermit.LogWriter
 import co.touchlab.kermit.MessageStringFormatter
 
 actual fun getPlatformLogWriter(formatter: MessageStringFormatter): LogWriter {
-    return CommonWriter(formatter)
+    return ConsoleWriter(formatter)
 }
