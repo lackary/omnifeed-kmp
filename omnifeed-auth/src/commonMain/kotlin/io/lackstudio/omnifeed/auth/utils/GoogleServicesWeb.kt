800f80e5fac3d9060ede1113b11838ec3a43288e
Yu-Chan (Henry) Huang <lackary@users.noreply.github.com>
2026-08-14 10:34:58 +0800

    refactor(auth): remove explicit SerialName annotations from GoogleServiceWeb
    Why:
    - To stick to Firebase web config rule and simplify the data class definition and rely on default property naming for serialization.
    
    What:
    - Removed `@SerialName` annotations from all properties in the `GoogleServiceWeb` data class, including `apiKey`, `authDomain`, `projectId`, `storageBucket`, `messagingSenderId`, `appId`, and `measurementId`.

--- omnifeed-auth/src/commonMain/kotlin/io/lackstudio/omnifeed/auth/utils/GoogleServicesWeb.kt
+++ omnifeed-auth/src/commonMain/kotlin/io/lackstudio/omnifeed/auth/utils/GoogleServicesWeb.kt
@@ -5,11 +5,11 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class GoogleServiceWeb(
-    @SerialName("api_key") val apiKey: String,
-    @SerialName("auth_domain") val authDomain: String,
-    @SerialName("project_id") val projectId: String,
-    @SerialName("storage_bucket") val storageBucket: String,
-    @SerialName("messaging_sender_id") val messagingSenderId: String,
-    @SerialName("app_id") val appId: String,
-    @SerialName("measurement_id") val measurementId: String
+    val apiKey: String,
+    val authDomain: String,
+    val projectId: String,
+    val storageBucket: String,
+    val messagingSenderId: String,
+    val appId: String,
+    val measurementId: String
 )
